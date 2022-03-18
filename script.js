640d7582c4e388da88a880b7ea4e3ff7e477a12f
lanaivanova <lana.ivanova@png.edu.lv>
2022-03-18 11:10:05 +0200

    zina

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log(1)
+//console.log(2)
+
+function sutitZinu()
+{
+    console.log('sutitZinu() darbojas');
+
+    let zina=document.querySelector('.manaZina');
+    let zinas=document.querySelector('.chataZinas');
+
+    zinas.innerHTML= zina.value;
+}
