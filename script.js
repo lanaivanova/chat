1e6e6d173ddc23f9c95f9cddb36a1179f58e21ef
lanaivanova <lana.ivanova@png.edu.lv>
2022-03-18 15:58:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let zina = document.querySelector('.manaZina');
 let zinas = document.querySelector('.chataZinas');
 let vards = document.querySelector('.vards');
 
+
 function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
 
     zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
 
-    fetch(API+'/sutit/'+ vards.value +'/'+zina.value)
+    fetch(API+'/sutit/'+vards.value+'/'+zina.value)
 }
 
 async function ieladetChataZinas()
@@ -19,19 +20,26 @@ async function ieladetChataZinas()
     zinas.innerHTML = dati;
 }
 
-
 //setInterval( ieladetChataZinas, 1000 )
 
 async function ieladetChataZinasJson()
 {
     let datiNoServera = await fetch(API + '/lasit');
     let dati = await datiNoServera.json();
-
-    //console.log(await dati[0]['zina'])
+    
+    //console.log(await dati[0]['zina'] )
+    zinas.innerHTML = '';
     
     i = 0;
     while ( i < await dati.length )
     {
-        console.log(i);
+        //console.log(i);
+        zinas.innerHTML = zinas.innerHTML+dati[i]['vards']+': '+dati[i]['zina']+'<br />';
+
         i = i+1;
     }
+
+    zinas.scrollTop = zinas.scrollHeight;
+}//beidzas ieladetChataZinasJson()
+
+setInterval( ieladetChataZinasJson, 1000 )
