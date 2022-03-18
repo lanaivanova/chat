a78381e26f3f82333d8f675b12db4b32ddfdeb61
lanaivanova <lana.ivanova@png.edu.lv>
2022-03-18 15:22:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const API= "https://chat.lanaivanova1.repl.co"
 let zina = document.querySelector('.manaZina');
 let zinas = document.querySelector('.chataZinas');
+let vards = document.querySelector('.vards');
 
 function sutitZinu()
 {
@@ -8,7 +9,7 @@ function sutitZinu()
 
     zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
 
-    fetch(API+'/sutit/Juris/'+zina.value)
+    fetch(API+'/sutit/'+ vards.value +'/'+zina.value)
 }
 
 async function ieladetChataZinas()
