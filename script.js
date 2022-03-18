02df837730714ccbc761cc47ce5baf8439f3ceb4
lanaivanova <lana.ivanova@png.edu.lv>
2022-03-18 12:02:00 +0200

    izmainas

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ let zinas = document.querySelector('.chataZinas');
 function sutitZinu()
 {
     console.log('sutitZinu() darbojas');
+
     zinas.innerHTML = zinas.innerHTML + '<br />' + zina.value;
 }
 
 async function ieladetChataZinas()
 {
-    let datiNoServera = await fetch('chatazinas.txt')
+    let datiNoServera = await fetch('chatazinas.txt');
     let dati = await datiNoServera.text();
-    zinas.innerHTML=dati;
-
+    zinas.innerHTML = dati;
 }
+
+setInterval( ieladetChataZinas, 1000 )
