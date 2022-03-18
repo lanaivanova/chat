d2336f43970bd319d47a0cb3555713333227e231
lanaivanova <lana.ivanova@png.edu.lv>
2022-03-18 16:56:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,13 +34,11 @@ async function ieladetChataZinasJson()
     while ( i < await dati.length )
     {
         //console.log(i);
-        if ("laiks" in dati[i]){
-            let laiks='<i>['+dati[i]['laiks'+'</i>]';
+        let laiks = '[<i>' + '????          ' + '</i>] ';
+        if ("laiks" in dati[i]) {
+            laiks = '[<i>' + dati[i]['laiks'] + '</i>] ';
         }
-        else {
-            let laiks='<i>['+'????         '+'</i>]';
-        }
-        zinas.innerHTML = zinas.innerHTML+laiks+dati[i]['vards']+': '+dati[i]['zina']+'<br />';
+        zinas.innerHTML = zinas.innerHTML + laiks + dati[i]['vards']+': '+dati[i]['zina']+'<br />';
 
         i = i+1;
     }
